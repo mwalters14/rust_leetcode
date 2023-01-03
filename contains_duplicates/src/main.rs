f9db3d8593914500cc518bc6821c8c5fc54bfd87
Myron Walters <myron.walters01@gmail.com>
2023-01-03 11:20:36 -0500

    contains_duplicate_solved_O(n)_O(n)

--- contains_duplicates/src/main.rs
+++ contains_duplicates/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashSet;
+use std::assert;
+
+fn contains_duplicates(nums: Vec<i32>) -> bool {
+    let mut set: HashSet<i32> = HashSet::new();
+
+    for num in nums.iter() {
+        if !set.insert(*num) {
+            return true;
+        }
+    }
+    false
 }
+fn main() {
+    assert!(contains_duplicates([1, 1, 3, 4, 5].to_vec()));
+}
+
+/*
+    Given an integer array nums, return true if any value appears 
+    at least twice in the array, and return false 
+    if every element is distinct.
+*/
---
+++ contains_duplicates/target/debug/deps/contains_duplicates-4a8f21ef01451ea4.d
@@ -0,0 +1,5 @@
+/Users/mwalters/git/rust_leetcode/contains_duplicates/target/debug/deps/contains_duplicates-4a8f21ef01451ea4.rmeta: src/main.rs
+
+/Users/mwalters/git/rust_leetcode/contains_duplicates/target/debug/deps/contains_duplicates-4a8f21ef01451ea4.d: src/main.rs
+
+src/main.rs:
